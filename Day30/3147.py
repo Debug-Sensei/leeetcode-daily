28041f17b4b59c15ca10e8d95c4476de8af5adc2
Harsh Kumar <harsh.kumar.8960367543@gmail.com>
2025-10-10 22:37:06 +0530

    feat: add Day 30 solution for Leetcode 3147

---
+++ Day30/3147.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumEnergy(self, energy, k):
+        n = len(energy)
+
+        for i in range(n - k - 1, -1, -1):
+            energy[i] += energy[i + k]
+
+        return max(energy)
